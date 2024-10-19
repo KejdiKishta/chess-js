08506ee2c9f307b0e4dd319881cf68293c4c9b73
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-10-19 15:42:11 +0200

    Add: pieces initial position

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // cell number
-let cellnr = 1;
+let cellnr = 64;
 
+//* CREATE BOARD
 // 8 rows
 for (let r = 0; r < 8; r++) {
     // foreach row 8 cells
@@ -8,10 +9,36 @@ for (let r = 0; r < 8; r++) {
 
         // create cell, add classes needed, append cell to the board
         const cell = document.createElement('div');
-        cell.className = 'cell ' + ((r + c) % 2 === 0 ? 'light-square ' : 'dark-square ') + cellnr;
+        cell.className = 'cell ' + ((r + c) % 2 === 0 ? 'light-square' : 'dark-square');
+        cell.setAttribute('data-cell', cellnr);
         document.querySelector('.board').appendChild(cell);
 
         // increment cell number
-        cellnr++;
+        cellnr--
+    }
+}
+
+//* CREATE PIECES INITIAL POSITION
+for (let c = 1; c <= 64; c++) {
+    const piece = document.createElement('div');
+    
+    if (c === 1 || c === 8 || c === 57 || c === 64) {
+        piece.textContent = 'rook'; 
+        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
+    } else if (c === 2 || c === 7 || c === 58 || c === 63) {
+        piece.textContent = 'knight'; 
+        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
+    } else if (c === 3 || c === 6 || c === 59 || c === 62) {
+        piece.textContent = 'bishop'; 
+        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
+    } else if (c === 4 || c === 60) {
+        piece.textContent = 'king'; 
+        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
+    }  else if (c === 5 || c === 61) {
+        piece.textContent = 'queen'; 
+        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
+    } else if ((c > 8 && c < 17) || (c > 48 && c < 57)) {
+        piece.textContent = 'pawn'; 
+        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
     }
 }
