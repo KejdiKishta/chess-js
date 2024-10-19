02a40fd3f4e45d430611fab7b0a6bb1ab206aa07
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-10-19 16:34:49 +0200

    add: pieces img and refactoring of cicle for

--- js/script.js
+++ js/script.js
@@ -20,25 +20,29 @@ for (let r = 0; r < 8; r++) {
 
 //* CREATE PIECES INITIAL POSITION
 for (let c = 1; c <= 64; c++) {
-    const piece = document.createElement('div');
+    let piece;
+
+    // create piece
+    if (c < 17 || c > 48) {
+        piece = document.createElement('div');
+    }
     
-    if (c === 1 || c === 8 || c === 57 || c === 64) {
-        piece.textContent = 'rook'; 
-        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
-    } else if (c === 2 || c === 7 || c === 58 || c === 63) {
-        piece.textContent = 'knight'; 
-        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
-    } else if (c === 3 || c === 6 || c === 59 || c === 62) {
-        piece.textContent = 'bishop'; 
-        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
-    } else if (c === 4 || c === 60) {
-        piece.textContent = 'king'; 
-        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
-    }  else if (c === 5 || c === 61) {
-        piece.textContent = 'queen'; 
-        document.querySelector(`[data-cell='${c}']`).appendChild(piece);
-    } else if ((c > 8 && c < 17) || (c > 48 && c < 57)) {
-        piece.textContent = 'pawn'; 
+    // define piece type and add piece
+    if (piece) {
+        if (c === 1 || c === 8 || c === 57 || c === 64) {
+            piece.className = (c === 1 || c === 8 ? 'white-rook' : 'black-rook');
+        } else if (c === 2 || c === 7 || c === 58 || c === 63) { 
+            piece.className = (c === 2 || c === 7 ? 'white-knight' : 'black-knight');
+        } else if (c === 3 || c === 6 || c === 59 || c === 62) {
+            piece.className = (c === 3 || c === 6 ? 'white-bishop' : 'black-bishop');
+        } else if (c === 4 || c === 60) {
+            piece.className = (c === 4 ? 'white-king' : 'black-king');
+        }  else if (c === 5 || c === 61) {
+            piece.className = (c === 5 ? 'white-queen' : 'black-queen')
+        } else if ((c > 8 && c < 17) || (c > 48 && c < 57)) {
+            piece.className = (c > 8 && c < 17 ? 'white-pawn' : 'black-pawn')
+        }
+
         document.querySelector(`[data-cell='${c}']`).appendChild(piece);
     }
 }
